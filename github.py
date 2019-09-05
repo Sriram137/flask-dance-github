d23b99c5b768ddc6edc4d890d74c0b3833d4c762
Sriram Ganesan <Sriram@rippling.com>
2019-09-05 13:37:48 -0700

    fix

--- github.py
+++ github.py
@@ -1,4 +1,5 @@
 import os
+from github import Github
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.github import make_github_blueprint, github
 
@@ -9,15 +10,20 @@ app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_S
 github_bp = make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
 
+
 @app.route("/hello")
 def index():
     return "/hello"
+
 
 @app.route("/")
 def login():
     if not github.authorized:
         return redirect(url_for("github.login"))
     resp = github.get("/user")
+    access_token = access_token
+    gh = Github(access_token)
+
     assert resp.ok
     return str(dir(github))
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
