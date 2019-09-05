45de3a723d7d3824b9b1efcbb71d425ce44b3aa2
Sriram Ganesan <Sriram@rippling.com>
2019-09-05 13:29:40 -0700

    test

--- github.py
+++ github.py
@@ -19,4 +19,8 @@ def login():
         return redirect(url_for("github.login"))
     resp = github.get("/user")
     assert resp.ok
+    return dir(github)
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
+
+def get_prs():
+
