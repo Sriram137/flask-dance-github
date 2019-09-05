0191289978ab98a8907d57ec83056b0969a48726
Sriram Ganesan <Sriram@rippling.com>
2019-09-05 13:41:26 -0700

    fix_more

--- github.py
+++ github.py
@@ -1,13 +1,14 @@
 import os
 from github import Github
 from flask import Flask, redirect, url_for
+import flask_dance.contrib.github
 from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
+github_bp = flask_dance.contrib.github.make_github_blueprint()
 app.register_blueprint(github_bp, url_prefix="/login")
 
 
@@ -18,16 +19,15 @@ def index():
 
 @app.route("/")
 def login():
-    if not github.authorized:
+    if not flask_dance.contrib.github.github.authorized:
         return redirect(url_for("github.login"))
-    resp = github.get("/user")
+    resp = flask_dance.contrib.github.github.get("/user")
     access_token = access_token
     gh = Github(access_token)
 
     return str(gh.get_organization("rippling"))
 
     assert resp.ok
-    return str(dir(github))
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
 
 # def get_prs():
